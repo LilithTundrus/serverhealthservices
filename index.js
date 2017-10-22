77718905bf02ca05cd4c5ff3f6b7ef87fdf2939a
Lilith Tundrus <echofox123@gmail.com>
2017-10-22 18:34:08 -0400

    added config.js as a require

--- index.js
+++ index.js
@@ -3,9 +3,13 @@
 'use strict';
 
 const fs = require('fs');                                               //fs will be used to read system files
+const config = require('./config');                                     //config file for script (file locations)
 var util = require('util');                                             //for using child-process-promise
 var exec = require('child-process-promise').exec;                       //promise-wrapped shell command execution
 var nodemailer = require('nodemailer');                                 //for sending emails
 
 
 //check if run as sudo for access to specific logs
+
+
+//detect which Linux OS: *bian or Arch or Cent/RHEL
