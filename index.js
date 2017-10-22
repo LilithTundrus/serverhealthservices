8d0824d953ed1e3a68ac9b4c4194fadc184876ad
Lilith Tundrus <echofox123@gmail.com>
2017-10-22 19:26:13 -0400

    added a isLinux() function for testing whether or not the host OS isLinux

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const fs = require('fs');                                               //fs wil
 const config = require('./config');                                     //config file for script (file locations)
 var isRoot = require('./lib/isRootCheck.js');                           //check if process is being run as root
 var util = require('util');                                             //for using child-process-promise
+var os = require('os');                                                 //for detecting if Linux
 var exec = require('child-process-promise').exec;                       //promise-wrapped shell command execution
 var nodemailer = require('nodemailer');                                 //for sending emails
 const ver = config.ver;                                                 //script version for debugging on deployments
+/*
+Notes: You MUST start this from the drive where /var/log/ dir is located!
 
+
+*/
+
+//check if host OS is in fact Linux
 //check if run as sudo for access to specific logs
 
 
@@ -19,18 +26,38 @@ const ver = config.ver;                                                 //script
 console.log(`Linux log monitor version ${ver} started at ${new Date().toISOString()}`);
 
 //checking for bad condition first (best way to do if statements)
-if (!isRoot()) {                                                    //if not root
-    console.log('Script is NOT running as root');                   //debugging
-
-    //check if /var/log/ exists
-    if (fs.existsSync('/var/log/') !== true) {
-        console.log('/var/log does not exist!');
-    } else {
-        //read some files
-        console.log('/var/log/ exists');
-    }
+if(!isLinux()) {
+    console.log('OS is NOT Linux, exiting');
+    process.exit(1);
 } else {
-    console.log('Script is running as root!');                      //debugging
-
+    if (!isRoot()) {                                                    //if not root
+        console.log('Script is NOT running as root');                   //debugging
+    
+        //check if /var/log/ exists
+        if (fs.existsSync('/var/log/') !== true) {
+            console.log('/var/log does not exist!');
+        } else {
+            //read some files
+            console.log('/var/log/ exists');
+        }
+    } else {
+        console.log('Script is running as root!');                      //debugging
+    
+    }
 }
+
 console.log('Script ended');                                        //debugging
+
+
+
+/**
+ * check if OS is linux, otherwise we can't do anything
+ * @returns {boolean}
+ */
+function isLinux() {
+    if (os.type() !== 'linux') {
+        return false;
+    } else {
+        return true;
+    }
+}
