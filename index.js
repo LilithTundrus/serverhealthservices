a1347c9c012f75b6992b7adce45e955234c5716f
Lilith Tundrus <echofox123@gmail.com>
2017-10-22 18:36:46 -0400

    added a testing console.log, updated comments

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 //Linux log / email /health graph here
 
 'use strict';
-
 const fs = require('fs');                                               //fs will be used to read system files
 const config = require('./config');                                     //config file for script (file locations)
 var util = require('util');                                             //for using child-process-promise
 var exec = require('child-process-promise').exec;                       //promise-wrapped shell command execution
 var nodemailer = require('nodemailer');                                 //for sending emails
-
+const ver = config.ver;                                                 //script version for debugging on deployments
 
 //check if run as sudo for access to specific logs
 
 
 //detect which Linux OS: *bian or Arch or Cent/RHEL
+
+
+//script starts here:
+console.log(`Linux log monitor version ${ver} started at ${new Date().toISOString()}`);
