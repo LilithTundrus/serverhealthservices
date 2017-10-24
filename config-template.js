6b8523d5eb27df8b0a6c44d300e4691af280de4d
Lilith Tundrus <echofox123@gmail.com>
2017-10-23 22:45:15 -0400

    updated config-template to include email placeholders for nodemailer

--- config-template.js
+++ config-template.js
@@ -2,3 +2,7 @@
 
 //arbitrary script version
 exports.ver = '0.0.1';
+//vars used for nodemailer to send server reports to a set email address
+exports.gmailSender = 'GMAIL_BOT_HERE'
+exports.gmailReceiver = 'SYSADMIN_EMAIL_HERE'
+exports.gmailPassword = 'GMAIL_SENDER_PASSWORD_HERE'
