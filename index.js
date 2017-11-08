9ee806dd01f7bca1814dfc3926674badbf0682b3
Lilith Tundrus <echofox123@gmail.com>
2017-11-07 23:47:05 -0500

    fixed document formatting

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function logHandler() {
             sendEmail(`New Log Activity at ${new Date().toISOString}`, logLocation + ':\n\n' + tailArray.join('\n'));
             tailArray = [];                                             //clear the array for the next interval
             return;
-        }, 10 * 60  * 1000);                                            //every 10 minutes
+        }, 10 * 60 * 1000);                                            //every 10 minutes
     });
 }
 
