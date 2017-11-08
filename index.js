baf0b00a481b318674f4fe00f22fd6b7e12080c7
Lilith Tundrus <echofox123@gmail.com>
2017-11-07 23:32:29 -0500

    Updated comments/formatting/JSHint suggestions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const config = require('./config');                                     //config
 var isRoot = require('./lib/isRootCheck.js');                           //check if process is being run as root
 var util = require('util');                                             //for using child-process-promise
 var os = require('os');                                                 //for detecting if Linux
-var nodemailer = require('nodemailer');                                 //for sending emails
+var nodemailer = require('nodemailer');                                 //for sending emails through node
 const Tail = require('nodejs-tail');                                    //for tailing files
 const ver = config.ver;                                                 //script version for debugging on deployments
 const logLocations = config.logLocationsArray;
@@ -13,10 +13,8 @@ Linux log / email / health graph thing...
 
 Notes: You MUST start this from the drive where /var/log/ dir is located!
 
-TODO: do more things with this
+TODO: do more things with this (graphs, insight/etc...)
 TODO: Extend logHandler to be generic
-TODO: include the log configuration as an array in config.js
-TODO: fix an invalid log location to not cause all items after to be removed!
 
 Script logical order:
 - check if host OS is in fact Linux and run as sudo (sanity check)
@@ -34,11 +32,12 @@ if (!isLinux()) {                                                       //check 
     process.exit(1);
 } else {
     if (!isRoot()) {                                                    //if not root
+        //the script can be run as a normal user but it can become a pain when dealing with permission issues
         console.log('Error: Script is must be run as root to avoid log access issues');
         process.exit(1);
     } else {
-        if (logDirCheck() !== true) {
-            console.log('/var/log/ does NOT exist! Exiting...');        //debugging
+        if (logDirCheck() !== true) {                                   //this should NEVER happen but is still a sanityt check
+            console.log('/var/log/ does NOT exist! Exiting...');
             return process.exit(1);
         }
         return logHandler();                                            //tail the logfiles
@@ -86,23 +85,20 @@ function logHandler() {
             //push tail line to array
             console.log(`${logLocation} new line: ${line}`);            //debugging
             tailArray.push(line);
-        })
-        tail.watch();
+        });
+        tail.watch();                                                   //"watch" the log files for changes
 
         //every so often send an email with the array
         setInterval(function () {
             if (tailArray.length == 0) {
                 return;                                                 //return, do nothing
             }
-            //beautify the log data:
+            //beautify the log data and send
             sendEmail(`New Log Activity at ${new Date().toISOString}`, logLocation + ':\n\n' + tailArray.join('\n'));
-            tailArray = [];                                             //clear the array
+            tailArray = [];                                             //clear the array for the next interval
             return;
-        }, 10 * 60  * 1000);                                         //every 10 minutes
-
-
+        }, 10 * 60  * 1000);                                            //every 10 minutes
     });
-
 }
 
 function sendEmail(subject, message) {                                  //send an email given the args to remove the duplicate code between beta an normal checks
