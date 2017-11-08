c513cb513c1efb2c6a11da96159282a8702e8568
Lilith Tundrus <echofox123@gmail.com>
2017-11-07 23:44:10 -0500

    fixed formatting/comments

--- config-template.js
+++ config-template.js
@@ -1,12 +1,9 @@
-//put configuration variables here as exports
-
 //arbitrary script version -- you don't have to change this
 exports.ver = '0.1.0';
 
 //vars used for nodemailer to send server reports to a set email address
-exports.gmailSender = 'GMAIL_BOT_HERE'
+exports.gmailSender = 'GMAIL_BOT_HERE';
 //password for the gmail sender (required)
-exports.gmailPassword = 'GMAIL_SENDER_PASSWORD_HERE'
-exports.gmailReceiver = 'SYSADMIN_EMAIL_HERE'
-exports.logLocationsArray = ['/var/log/log1.log', '/var/log/log2.log']
-
+exports.gmailPassword = 'GMAIL_SENDER_PASSWORD_HERE';
+exports.gmailReceiver = 'SYSADMIN_EMAIL_HERE';
+exports.logLocationsArray = ['/var/log/log1.log', '/var/log/log2.log'];
